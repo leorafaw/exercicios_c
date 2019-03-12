a6ac1d82d9f87231ca3d1c1286ec361ed0135102
Leonardo <leonardo@agilsoft.local>
2019-03-12 17:50:26 -0300

    Update parte2-4.c

--- parte2-4.c
+++ parte2-4.c
@@ -3,5 +3,20 @@
 #include <string.h>
 main()
 {
+    float peso, precoKG, total, liquido;
 
+    printf("Informe o peso comprado: ");
+    scanf("%f", &peso);
+    printf("Informe o preco por KG: ");
+    scanf("%f", &precoKG);
+
+    total = peso*precoKG;
+
+    printf("TOTAL A PAGAR: %.2f", total);
+
+    liquido = total*0.83;
+
+    printf("\nTOTAL LIQUIDO: %.2f", liquido);
+
+    return 0;
 }
